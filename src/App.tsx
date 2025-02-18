271074d8d90e58bdb0e7dd278fd538e56fda10fd
Victor <victor.dmaina@gmail.com>
2025-02-18 09:31:20 +0300

    install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='bg-red-500'>
+      <div className='bg-blue-500'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
